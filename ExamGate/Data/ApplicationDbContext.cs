6704b41cc9efacd79c2ec9e67d69cfdd03acdd0d
KalCode <60557423+ESCANORssama-tech@users.noreply.github.com>
2022-03-26 12:55:44 +0200

    Initial commit sto master

---
+++ ExamGate/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ExamGate.Models;
+
+
+
+namespace ExamGate.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        //public DbSet<User> User { get; set; }
+    }
+}
