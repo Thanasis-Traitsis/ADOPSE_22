fd7b5fb08ef429e2209cdeab2fbec35d500f4abb
Thanasis-Traitsis <thanasistraitsis@gmail.com>
2022-04-03 18:49:17 +0300

    login_signup

--- ExamGate/Program.cs
+++ ExamGate/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=LogIn}/{action=Index}/{id?}");
 
 app.Run();
